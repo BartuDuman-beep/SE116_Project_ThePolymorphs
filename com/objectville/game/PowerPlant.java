8d4af9d9b79bf2b47fbc54053151c669b53cfee6
dogacanakkale <dogacanakkale@icloud.com>
2026-05-18 12:08:05 +0300

    Second commit:UtilityBuilding, PowerPlant,WaterStation,InternetHub are added.

---
+++ com/objectville/game/PowerPlant.java
@@ -0,0 +1,7 @@
+package com.objectville.game;
+
+public class PowerPlant extends UtilityBuilding {
+    public PowerPlant(String utilityType, int capacity,int remainingCapacity, int row, int column) {
+        super("electricity", 100,remainingCapacity, row, column);
+    }
+}
