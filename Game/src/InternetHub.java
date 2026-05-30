e22f6f58fea283465f313b07954f5d5a24ff6ced
melisanrg <melisa.nergiz@std.izmirekonomi.edu.tr>
2026-05-30 22:24:15 +0300

    update to MapReader

--- Game/src/InternetHub.java
+++ Game/src/InternetHub.java
@@ -1,7 +1,6 @@
 
-public class InternetHub extends UtilityBuilding{
-    public InternetHub(int x,int y) {
-    super(x, y, 'T',"internet",100,100);
+public class InternetHub extends UtilityBuilding{public InternetHub(String utilityType, int capacity,int remainingCapacity, int row, int column) {
+    super("internet", 100 , remainingCapacity, row, column);
 }
 
 }
---
+++ Game/src/MapReader.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class MapReader {
+    private int rows;
+    private int cols;
+    private Cell[][] grid;
+
+}
