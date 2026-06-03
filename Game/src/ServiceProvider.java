62e0488f18fa542739c95a1e9ed543d3cabd94bd
melisanrg <melisa.nergiz@std.izmirekonomi.edu.tr>
2026-06-03 13:03:36 +0300

    update to ServiceProvider

--- Game/src/ServiceProvider.java
+++ Game/src/ServiceProvider.java
@@ -1,5 +1,10 @@
 
-public interface ServiceProvider {
-    String getServiceType();
-    int getRadius();
+public abstract class ServiceProvider extends Cell {
+    public char serviceType;
+    protected int radius;
+
+    public ServiceProvider(int x, int y, char symbol) {
+        super(x, y, symbol);
+
+    }
 }
