1dcc38a9a9f3ec02f0544475851ca9ed86fa0da5
roksandeveci <elbim.roksan.deveci@gmail.com>
2026-06-03 18:39:27 +0300

    Add Industrial.java

---
+++ Game/src/Industrial.java
@@ -0,0 +1,44 @@
+public class Industrial extends Zone {
+    public Industrial(int x, int y) {
+        super(x, y, 'I');
+    }
+    @Override
+    public void updateLevel() {
+        boolean hasUtilities = electricity > 0 && water > 0;
+        if (!hasUtilities) {
+            dropToZero();
+            return;
+        }
+        boolean canReachLevel1 = population > 0;
+        boolean canReachLevel2 = canReachLevel1 && security;
+        boolean canReachLevel3 = canReachLevel2 && population > 1;
+        int targetLevel;
+        if (canReachLevel3) {
+            targetLevel = 3;
+        } else if (canReachLevel2) {
+            targetLevel = 2;
+        } else if (canReachLevel1) {
+            targetLevel = 1;
+        } else {
+            targetLevel = 0;
+        }
+        if (targetLevel > level) {
+            increaseLevel();
+        } else if (targetLevel < level) {
+            decreaseLevel();
+        }
+    }
+    @Override
+    public void calculateOutput() {
+        int m = Math.min(electricity, water);
+        if (level == 0) {
+            output = 0;
+        } else if (level == 1) {
+            output = m;
+        } else if (level == 2) {
+            output = 2 * m;
+        } else {
+            output = (2 * m) + population;
+        }
+    }
+}
