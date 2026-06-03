00ff9b786b3d123c48aefc31467bf5d40c2b6782
Bartu Duman <bartu.duman@std.izmirekonomi.edu.tr>
2026-06-03 22:23:27 +0300

    Main continued

--- Game/src/Main.java
+++ Game/src/Main.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,5 +11,23 @@ public class Main {
         List<String> lines = new ArrayList<>();
         String fileName = args[0];
         int totalTicks = Integer.parseInt(args[1]);
+        try {
+            MapReader mapReader = new MapReader(fileName);
+
+            int rows = mapReader.getRows();
+            int cols = mapReader.getCols();
+            MapReader city = new MapReader(fileName);
+
+            Cell[][] grid = mapReader.getGrid();
+
+            for (int y = 0; y < rows; y++) {
+                for (int x = 0; x < cols; x++) {
+                    //a
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("ERROR: " + fileName + " A problem occurred while reading the file.");
+            System.out.println("Details: " + e.getMessage());
+        }
     }
 }
