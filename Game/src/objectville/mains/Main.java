2141fdcac31013896f060d667489b791cecfeb54
Bartu Duman <bartu.duman@std.izmirekonomi.edu.tr>
2026-06-04 19:41:30 +0300

    Main-Simulation problem solved

--- Game/src/objectville/mains/Main.java
+++ Game/src/objectville/mains/Main.java
@@ -19,7 +19,7 @@ public class Main {
             CityGrid cityGrid = new CityGrid(grid);
             Simulation simulation = new Simulation(grid, cityGrid);
             for (int t = 0; t < totalTicks; t++) {
-                simulation.nextTick();
+                simulation.runTick();
             }
 
         } catch (IOException e) {
