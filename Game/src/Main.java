ddbc88bff5ca66aa367b04d6cc85f1a4720f3043
Bartu Duman <bartu.duman@std.izmirekonomi.edu.tr>
2026-06-04 18:41:19 +0300

    Main and Services updated

--- Game/src/Main.java
+++ Game/src/Main.java
@@ -1,6 +1,4 @@
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,23 +6,17 @@ public class Main {
             System.out.println("Pls enter file name and tick number.");
             return;
         }
-        List<String> lines = new ArrayList<>();
         String fileName = args[0];
         int totalTicks = Integer.parseInt(args[1]);
         try {
             MapReader mapReader = new MapReader(fileName);
-
-            int rows = mapReader.getRows();
-            int cols = mapReader.getCols();
-            MapReader city = new MapReader(fileName);
-
             Cell[][] grid = mapReader.getGrid();
-
-            for (int y = 0; y < rows; y++) {
-                for (int x = 0; x < cols; x++) {
-                    //a
-                }
+            CityGrid cityGrid = new CityGrid(grid);
+            Simulation simulation = new Simulation(grid, cityGrid);
+            for (int t = 0; t < totalTicks; t++) {
+                simulation.nextTick();
             }
+
         } catch (IOException e) {
             System.out.println("ERROR: " + fileName + " A problem occurred while reading the file.");
             System.out.println("Details: " + e.getMessage());
